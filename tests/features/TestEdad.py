53fc1714c41c1445e8d9c602eea0e7e82338c531
Gilberto Estrada <estradaluna.gilberto@gmail.com>
2017-10-04 23:01:06 -0500

    Se eliminan los guiones bajos del nombre de funcion obtener_resultado
    Se prentendía utilizar la convención de código de python para
    declarar la función obtener_resultado al agregar dos guiones
    bajos al inicio y al final, ahora se intenta sin el uso
    de este par de guiones bajos

--- tests/features/TestEdad.py
+++ tests/features/TestEdad.py
@@ -9,34 +9,34 @@ class TestEdad(unittest.TestCase):
         self.edad = Edad()
 
     def test_si_tienes_neg_5_anios_no_existes(self):
-        self.assertEquals(self.__obtener_resultado__(-5),
+        self.assertEquals(self.obtener_resultado(-5),
                             u'No existes')
 
     def test_si_tienes_0_anios_eres_bebe(self):
-        self.assertEquals(self.__obtener_resultado__(0),
+        self.assertEquals(self.obtener_resultado(0),
                             u'Eres un bebe')
 
     def test_si_tienes_8_anios_eres_ninio(self):
-        self.assertEquals(self.__obtener_resultado__(8),
+        self.assertEquals(self.obtener_resultado(8),
                             u'Eres un nino')
 
     def test_si_tienes_15_anios_eres_adolescente(self):
-        self.assertEquals(self.__obtener_resultado__(15),
+        self.assertEquals(self.obtener_resultado(15),
                             u'Eres un adolescente')
 
     def test_si_tienes_50_anios_eres_adulto(self):
-        self.assertEquals(self.__obtener_resultado__(50),
+        self.assertEquals(self.obtener_resultado(50),
                             u'Eres un adulto')
 
     def test_si_tienes_100_anios_eres_adulto_mayor(self):
-        self.assertEquals(self.__obtener_resultado__(100),
+        self.assertEquals(self.obtener_resultado(100),
                             u'Eres un adulto mayor')
 
     def test_si_tienes_275_anios_eres_mummra(self):
-        self.assertEquals(self.__obtener_resultado__(275),
+        self.assertEquals(self.obtener_resultado(275),
                             u'Eres Mumm-Ra')
 
-    def __obtener_resultado__(self, edad):
+    def obtener_resultado(self, edad):
         return self.edad.evaluar_edad(edad)
 
     def tearDown(self):
